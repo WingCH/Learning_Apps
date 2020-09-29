aa5cb3837cd978a5974f71ce27c55ae4680077b9
WingCH <jason021075@me.com>
2020-09-29 21:21:45 +0800

    init new project

---
+++ how-to-use-the-coordinator-pattern-in-ios-apps/how-to-use-the-coordinator-pattern-in-ios-apps/Coordinator.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinator.swift
+//  how-to-use-the-coordinator-pattern-in-ios-apps
+//
+//  Created by CHAN Hong Wing on 29/9/2020.
+//  Copyright © 2020 CHAN Hong Wing. All rights reserved.
+//
+
+import Foundation
---
+++ how-to-use-the-coordinator-pattern-in-ios-apps/how-to-use-the-coordinator-pattern-in-ios-apps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  how-to-use-the-coordinator-pattern-in-ios-apps
+//
+//  Created by CHAN Hong Wing on 29/9/2020.
+//  Copyright © 2020 CHAN Hong Wing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
