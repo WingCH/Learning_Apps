55b9a508dc499bebdf286ee07db372a3c4475030
WingCH <jason021075@me.com>
2020-10-04 21:44:41 +0800

    add Custom URL Scheme

--- ApplicationCoordinator/ApplicationCoordinator/Application/AppDelegate.swift
+++ ApplicationCoordinator/ApplicationCoordinator/Application/AppDelegate.swift
@@ -6,7 +6,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return self.window!.rootViewController as! UINavigationController
   }
   
+    // https://medium.com/wolox/ios-deep-linking-url-scheme-vs-universal-links-50abd3802f97
+    // URL Scheme:  com.myApp://xxx
+    // Quick way to open a Custom URL Scheme in iOS Simulator
+    // https://sarunw.com/posts/quick-way-to-open-custom-url-scheme-in-ios-simulator/
   private lazy var applicationCoordinator: Coordinator = self.makeCoordinator()
+    
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if let scheme = url.scheme,
+            scheme.localizedCaseInsensitiveCompare("com.myApp") == .orderedSame,
+            let view = url.host {
+            
+            var parameters: [String: String] = [:]
+            URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?.forEach {
+                parameters[$0.name] = $0.value
+            }
+            
+            print(view);
+            print(parameters);
+            applicationCoordinator.start(with: DeepLinkOption.onboarding)
+        }
+        return true
+    }
   
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
