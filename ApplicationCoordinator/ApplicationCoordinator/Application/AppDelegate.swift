98de65954b10a0142076c11432cefe72d4bb7141
WingCH <wingchan@vgt-hk.net>
2020-10-05 16:08:42 +0800

    format

--- ApplicationCoordinator/ApplicationCoordinator/Application/AppDelegate.swift
+++ ApplicationCoordinator/ApplicationCoordinator/Application/AppDelegate.swift
@@ -1,16 +1,16 @@
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  
-  var window: UIWindow?
-  var rootController: UINavigationController {
-    return self.window!.rootViewController as! UINavigationController
-  }
-  
+    
+    var window: UIWindow?
+    var rootController: UINavigationController {
+        return self.window!.rootViewController as! UINavigationController
+    }
+    
     // https://medium.com/wolox/ios-deep-linking-url-scheme-vs-universal-links-50abd3802f97
     // URL Scheme:  com.myApp://xxx
     // Quick way to open a Custom URL Scheme in iOS Simulator
     // https://sarunw.com/posts/quick-way-to-open-custom-url-scheme-in-ios-simulator/
-  private lazy var applicationCoordinator: Coordinator = self.makeCoordinator()
+    private lazy var applicationCoordinator: Coordinator = self.makeCoordinator()
     
     func application(_ app: UIApplication, open url: URL,
                      options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
@@ -29,34 +29,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
-  
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    let notification = launchOptions?[.remoteNotification] as? [String: AnyObject]
-    let deepLink = DeepLinkOption.build(with: notification)
-    applicationCoordinator.start(with: deepLink)
-    return true
-  }
-  
-  private func makeCoordinator() -> Coordinator {
-      return ApplicationCoordinator(
-        router: RouterImp(rootController: self.rootController),
-        coordinatorFactory: CoordinatorFactoryImp()
-      )
-  }
-  
-  //MARK: Handle push notifications and deep links
-  func application(_ application: UIApplication,
-                   didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
-    let dict = userInfo as? [String: AnyObject]
-    let deepLink = DeepLinkOption.build(with: dict)
-    applicationCoordinator.start(with: deepLink)
-  }
-  
-  func application(_ application: UIApplication, continue userActivity: NSUserActivity,
-                   restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-    let deepLink = DeepLinkOption.build(with: userActivity)
-    applicationCoordinator.start(with: deepLink)
-    return true
-  }
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let notification = launchOptions?[.remoteNotification] as? [String: AnyObject]
+        let deepLink = DeepLinkOption.build(with: notification)
+        applicationCoordinator.start(with: deepLink)
+        return true
+    }
+    
+    private func makeCoordinator() -> Coordinator {
+        return ApplicationCoordinator(
+            router: RouterImp(rootController: self.rootController),
+            coordinatorFactory: CoordinatorFactoryImp()
+        )
+    }
+    
+    //MARK: Handle push notifications and deep links
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        let dict = userInfo as? [String: AnyObject]
+        let deepLink = DeepLinkOption.build(with: dict)
+        applicationCoordinator.start(with: deepLink)
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity,
+                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        let deepLink = DeepLinkOption.build(with: userActivity)
+        applicationCoordinator.start(with: deepLink)
+        return true
+    }
 }
