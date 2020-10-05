83c760af5d1e470159600b97157edfc1d06add73
WingCH <wingchan@vgt-hk.net>
2020-10-05 14:07:39 +0800

    add format

--- ApplicationCoordinator/ApplicationCoordinator/Flows/Items flow/ItemsListView.swift
+++ ApplicationCoordinator/ApplicationCoordinator/Flows/Items flow/ItemsListView.swift
@@ -1,4 +1,4 @@
 protocol ItemsListView: BaseView {
-  var onItemSelect: ((ItemList) -> ())? { get set }
-  var onCreateItem: (() -> Void)? { get set }
+    var onItemSelect: ((ItemList) -> ())? { get set }
+    var onCreateItem: (() -> Void)? { get set }
 }
