2466f9a70900e7df2b63b2d4448d8ec3759baf63
WingCH <wingchan@vgt-hk.net>
2020-10-28 16:10:37 +0800

    switch theme ui

--- DarkModeDemo/DarkModeDemo/Sources/UI/SettingsViewController.swift
+++ DarkModeDemo/DarkModeDemo/Sources/UI/SettingsViewController.swift
@@ -7,33 +7,85 @@ import Foundation
 import UIKit
 
 class SettingsViewController: UITableViewController {
-
-    @IBOutlet private weak var cell: UITableViewCell!
-    private let themeSwitch = UISwitch()
+    
+    @IBOutlet private weak var defaultCell: UITableViewCell!
+    @IBOutlet private weak var lightCell: UITableViewCell!
+    @IBOutlet private weak var darkCell: UITableViewCell!
+    
+    private let defaultThemeSwitch = UISwitch()
+    private let lightThemeSwitch = UISwitch()
+    private let darkThemeSwitch = UISwitch()
+    
     var themeProvider: ThemeProvider?
     
     override func viewDidLoad() {
-	    super.viewDidLoad()
-	    themeSwitch.addTarget(self, action: #selector(didTap(switchView:)), for: .valueChanged)
-	    cell.accessoryView = themeSwitch
-	    tableView.tableFooterView = UIView()
-	    themeProvider?.register(observer: self)
+        super.viewDidLoad()
+        defaultThemeSwitch.addTarget(self, action: #selector(didTap(switchView:)), for: .valueChanged)
+        defaultCell.accessoryView = defaultThemeSwitch
+        
+        lightThemeSwitch.addTarget(self, action: #selector(didTap(switchView:)), for: .valueChanged)
+        lightCell.accessoryView = lightThemeSwitch
+        
+        darkThemeSwitch.addTarget(self, action: #selector(didTap(switchView:)), for: .valueChanged)
+        darkCell.accessoryView = darkThemeSwitch
+        
+        tableView.tableFooterView = UIView()
+        themeProvider?.register(observer: self)
     }
-
+    
     @objc func didTap(switchView: UISwitch) {
-	    themeProvider?.toggleTheme()
+        switch switchView {
+        case defaultThemeSwitch:
+            if defaultThemeSwitch.isOn {
+                lightThemeSwitch.setOn(false, animated: true)
+                darkThemeSwitch.setOn(false, animated: true)
+            }else{
+                lightThemeSwitch.setOn(true, animated: true)
+            }
+            break
+        case lightThemeSwitch:
+            if lightThemeSwitch.isOn {
+                defaultThemeSwitch.setOn(false, animated: true)
+                darkThemeSwitch.setOn(false, animated: true)
+            }else{
+                defaultThemeSwitch.setOn(true, animated: true)
+            }
+            break
+        case darkThemeSwitch:
+            if darkThemeSwitch.isOn {
+                defaultThemeSwitch.setOn(false, animated: true)
+                lightThemeSwitch.setOn(false, animated: true)
+            }else{
+                defaultThemeSwitch.setOn(true, animated: true)
+            }
+            break
+        default:
+            break
+        }
+        //	    themeProvider?.toggleTheme()
     }
 }
 
 extension SettingsViewController: Themeable {
     func apply(theme: Theme) {
-	    tableView.backgroundColor = theme.tableViewBackground
-	    tableView.separatorColor = theme.separatorColor
-
-	    cell.backgroundColor = theme.backgroundColor
-	    cell.textLabel?.textColor = theme.textColor
-	    themeSwitch.onTintColor = theme.switchTintColor
-	    themeSwitch.isOn = theme == .dark
+        tableView.backgroundColor = theme.tableViewBackground
+        tableView.separatorColor = theme.separatorColor
+        
+        defaultCell.backgroundColor = theme.backgroundColor
+        defaultCell.textLabel?.textColor = theme.textColor
+        defaultThemeSwitch.onTintColor = theme.switchTintColor
+        defaultThemeSwitch.isOn = theme == .dark
+        
+        
+        lightCell.backgroundColor = theme.backgroundColor
+        lightCell.textLabel?.textColor = theme.textColor
+        lightThemeSwitch.onTintColor = theme.switchTintColor
+        lightThemeSwitch.isOn = theme == .dark
+        
+        darkCell.backgroundColor = theme.backgroundColor
+        darkCell.textLabel?.textColor = theme.textColor
+        darkThemeSwitch.onTintColor = theme.switchTintColor
+        darkThemeSwitch.isOn = theme == .dark
     }
-
+    
 }
