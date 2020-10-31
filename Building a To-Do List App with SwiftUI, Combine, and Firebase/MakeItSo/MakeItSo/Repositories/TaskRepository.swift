2e03aa19c03c6a65c583b26ea7ae311df5fd1dee
WingCH <jason021075@me.com>
2020-10-31 11:06:57 +0800

    28:37 min Part 2

---
+++ Building a To-Do List App with SwiftUI, Combine, and Firebase/MakeItSo/MakeItSo/Repositories/TaskRepository.swift
@@ -0,0 +1,36 @@
+//
+//  TaskRepository.swift
+//  MakeItSo
+//
+//  Created by CHAN Hong Wing on 30/10/2020.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class TaskRepository: ObservableObject {
+    let db = Firestore.firestore()
+    
+    @Published var tasks = [Task]()
+    
+    init() {
+        loadData()
+    }
+    
+    func loadData() {
+        db.collection("tasks").addSnapshotListener { (querySnapshot, error) in
+            if let querySnapshot = querySnapshot {
+                self.tasks = querySnapshot.documents.compactMap({ document in
+                    do {
+                        let x = try document.data(as: Task.self)
+                        return x
+                    }catch {
+                        print(error)
+                    }
+                    return nil
+                })
+            }
+        }
+    }
+}
--- Building a To-Do List App with SwiftUI, Combine, and Firebase/MakeItSo/MakeItSo/ViewModels/TaskListViewModel.swift
+++ Building a To-Do List App with SwiftUI, Combine, and Firebase/MakeItSo/MakeItSo/ViewModels/TaskListViewModel.swift
@@ -8,16 +8,21 @@
 import Foundation
 import Combine
 
-// https://github.com/schmidyy/SwiftUI-ListFetching/issues/3#issuecomment-528673860
 class TaskListViewModel: ObservableObject {
+    @Published var taskRepository = TaskRepository()
     @Published var taskCellViewModels = [TaskCellViewModel]()
     
     private var cancellables = Set<AnyCancellable>()
     
     init() {
-        self.taskCellViewModels = testDataTasks.map({ task in
-            TaskCellViewModel(task: task)
-        })
+        taskRepository.$tasks.map { tasks in
+            tasks.map { task in
+                TaskCellViewModel(task: task)
+            }
+        }
+        .assign(to: \.taskCellViewModels, on: self)
+        .store(in: &cancellables)
+        
     }
     
     func addTask(task: Task) {
