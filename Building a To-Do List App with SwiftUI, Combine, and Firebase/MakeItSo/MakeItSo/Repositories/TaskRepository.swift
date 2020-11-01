c4c15bbb77174cd841bd0c8f0d2576bc28637336
WingCH <jason021075@me.com>
2020-11-01 11:29:48 +0800

    46:50 min Part 2

--- Building a To-Do List App with SwiftUI, Combine, and Firebase/MakeItSo/MakeItSo/Repositories/TaskRepository.swift
+++ Building a To-Do List App with SwiftUI, Combine, and Firebase/MakeItSo/MakeItSo/Repositories/TaskRepository.swift
@@ -22,26 +22,38 @@ class TaskRepository: ObservableObject {
         db.collection("tasks")
             .order(by: "createdTime")
             .addSnapshotListener { (querySnapshot, error) in
-            if let querySnapshot = querySnapshot {
-                self.tasks = querySnapshot.documents.compactMap({ document in
-                    do {
-                        let x = try document.data(as: Task.self)
-                        return x
-                    }catch {
-                        print(error)
-                    }
-                    return nil
-                })
+                if let querySnapshot = querySnapshot {
+                    self.tasks = querySnapshot.documents.compactMap({ document in
+                        do {
+                            let x = try document.data(as: Task.self)
+                            return x
+                        }catch {
+                            print(error)
+                        }
+                        return nil
+                    })
+                }
             }
-        }
     }
     
     func addTask(_ task: Task){
         do {
             let _ = try db.collection("tasks").addDocument(from: task)
-
+            
         } catch {
             fatalError("Unable to encode task: \(error.localizedDescription)")
+        }
+    }
+    
+    func updateTask(_ task: Task){
+        if let taskID = task.id {
+            do {
+                try db.collection("tasks")
+                    .document(taskID)
+                    .setData(from: task)
+            } catch {
+                fatalError("Unable to encode task: \(error.localizedDescription)")
+            }
         }
     }
 }
