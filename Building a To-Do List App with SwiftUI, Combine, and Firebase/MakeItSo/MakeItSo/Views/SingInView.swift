a089c2be6259d7103d9a135d986f1de305872356
WingCH <jason021075@me.com>
2020-11-01 16:57:23 +0800

    32min part3

--- Building a To-Do List App with SwiftUI, Combine, and Firebase/MakeItSo/MakeItSo/Views/SingInView.swift
+++ Building a To-Do List App with SwiftUI, Combine, and Firebase/MakeItSo/MakeItSo/Views/SingInView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct SingInView: View {
     @Environment(\.presentationMode) var presentationMode
+    @State var coordinator:SignInWithAppleCoordinator?
+    
     var body: some View {
         VStack {
             Text("Thanks for using Make It So. Please sign in here")
             SignInWithAppleButtin()
                 .frame(width: 288, height: 45)
                 .onTapGesture(count: 1, perform: {
-                    self.presentationMode.wrappedValue.dismiss()
+                    self.coordinator = SignInWithAppleCoordinator()
+                    if let coordinator = self.coordinator {
+                        coordinator.startSignInWithAppleFlow {
+                            print("You successfully signed in")
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                    }
                 })
             
         }
