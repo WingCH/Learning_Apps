d00d25c4896e7f8a618872c256cb8c78202297e8
WingCH <jason021075@me.com>
2020-11-29 19:55:37 +0800

    extract work part

--- Swinject Tutorial for iOS - Getting Started/BitcoinAdventurer-Starter/Bitcoin Adventurer/Dependencies/HTTPNetworking.swift
+++ Swinject Tutorial for iOS - Getting Started/BitcoinAdventurer-Starter/Bitcoin Adventurer/Dependencies/HTTPNetworking.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2018 Razeware LLC
-///
+/// Copyright (c) 2020 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,17 +26,37 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-/*
- {
-   "data": {
-     "base": "BTC",
-     "currency": "USD",
-     "amount": "15840.01"
-   }
- }
+import Foundation
 
- */
-struct PriceResponse: Codable {
-  let data: Price
-  let warnings: [Error]?
+// 1.
+protocol Networking {
+  typealias CompletionHandler = (Data?, Swift.Error?) -> Void
+
+  func request(from: Endpoint, completion: @escaping CompletionHandler)
+}
+
+// 2.
+struct HTTPNetworking: Networking {
+  // 3.
+  func request(from: Endpoint, completion: @escaping CompletionHandler) {
+    guard let url = URL(string: from.path) else { return }
+    let request = createRequest(from: url)
+    let task = createDataTask(from: request, completion: completion)
+    task.resume()
+  }
+
+  // 4.
+  private func createRequest(from url: URL) -> URLRequest {
+    var request = URLRequest(url: url)
+    request.cachePolicy = .reloadIgnoringCacheData
+    return request
+  }
+
+  // 5.
+  private func createDataTask(from request: URLRequest,
+                              completion: @escaping CompletionHandler) -> URLSessionDataTask {
+    return URLSession.shared.dataTask(with: request) { data, httpResponse, error in
+      completion(data, error)
+    }
+  }
 }
