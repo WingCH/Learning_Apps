6933e820e778f21f363f604740b9b4243ee99917
WingCH <wingchan@vgt-hk.net>
2021-01-13 17:09:14 +0800

    add Firebase emulator example

---
+++ friendlyeats-ios/FriendlyEats/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  Copyright (c) 2016 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    
+    // Firestore
+    let settings = Firestore.firestore().settings
+    settings.host = "localhost:8080"
+    settings.isPersistenceEnabled = false
+    settings.isSSLEnabled = false
+    Firestore.firestore().settings = settings
+    // Auth
+//    Auth.auth().useEmulator(withHost:"localhost", port:9099)
+    return true
+  }
+
+}
+
