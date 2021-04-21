c35872b8e6d40c53f5cc8cd351c1250d629f40ec
WingCH <jason021075@me.com>
2021-04-21 19:22:17 +0800

    add AddItemsOnTopAndBottomOfSliverPage

---
+++ Flutter/top_bottom_add_items_sliver/lib/add_items_on_top_and_bottom_of_sliver_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class AddItemsOnTopAndBottomOfSliverPage extends StatefulWidget {
+  AddItemsOnTopAndBottomOfSliverPage({Key key}) : super(key: key);
+
+  @override
+  _AddItemsOnTopAndBottomOfSliverPageState createState() =>
+      _AddItemsOnTopAndBottomOfSliverPageState();
+}
+
+class _AddItemsOnTopAndBottomOfSliverPageState
+    extends State<AddItemsOnTopAndBottomOfSliverPage> {
+  List<int> top = [];
+  List<int> bottom = [0];
+
+  @override
+  Widget build(BuildContext context) {
+    const Key centerKey = ValueKey('second-sliver-list');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Press on the plus to add items above and below'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                top.add(-top.length - 1);
+                bottom.add(bottom.length);
+              });
+            },
+          )
+        ],
+      ),
+      body: CustomScrollView(
+        center: centerKey,
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.blue[200 + top[index] % 4 * 100],
+                  height: 100,
+                  child: Text('Item: ${top[index]}'),
+                );
+              },
+              childCount: top.length,
+            ),
+          ),
+          SliverList(
+            key: centerKey,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.blue[200 + bottom[index] % 4 * 100],
+                  height: 100 + bottom[index] % 4 * 20.0,
+                  child: Text('Item: ${bottom[index]}'),
+                );
+              },
+              childCount: bottom.length,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
