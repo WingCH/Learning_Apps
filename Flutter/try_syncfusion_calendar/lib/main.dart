5f300efef5b5d769eb437260634bbeaa541a8be8
WingCH <jason021075@me.com>
2021-06-11 20:31:05 +0800

    init

--- Flutter/try_syncfusion_calendar/lib/main.dart
+++ Flutter/try_syncfusion_calendar/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,68 +47,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: AppBar(),
+      body: Container(
+        // height: 500,
+        child: SfCalendar(
+          view: CalendarView.month,
+          showNavigationArrow: true,
+          dataSource: MeetingDataSource(_getDataSource()),
+          monthViewSettings: MonthViewSettings(
+            appointmentDisplayMode: MonthAppointmentDisplayMode.indicator,
+            showAgenda: true,
+            agendaViewHeight: 400,
+            agendaItemHeight: 70,
+            dayFormat: 'EEE',
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  List<Appointment> _getDataSource() {
+    final List<Appointment> meetings = <Appointment>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime =
+        DateTime(today.year, today.month, today.day + 1, 9, 0, 0);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+    meetings.add(
+      Appointment(
+        startTime: startTime,
+        endTime: endTime,
+        subject: 'Conference',
+        color: Color(0xFF0F8644),
+        isAllDay: false,
+      ),
+    );
+
+    // meetings.add(Meeting(
+    //     'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+    return meetings;
+  }
 }
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+
+// @override
+// DateTime getStartTime(int index) {
+//   return appointments![index].from;
+// }
+//
+// @override
+// DateTime getEndTime(int index) {
+//   return appointments![index].to;
+// }
+//
+// @override
+// String getSubject(int index) {
+//   return appointments![index].eventName;
+// }
+//
+// @override
+// Color getColor(int index) {
+//   return appointments![index].background;
+// }
+//
+// @override
+// bool isAllDay(int index) {
+//   return appointments![index].isAllDay;
+// }
+}
+
+// class Meeting {
+//   Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+//
+//   String eventName;
+//   DateTime from;
+//   DateTime to;
+//   Color background;
+//   bool isAllDay;
+// }
